5e5d2620ad00135e4c356ba898112d8f72246081
BOJJA RAJI <94085980+BOJJRAJI@users.noreply.github.com>
2023-09-13 19:17:38 +0530

    Update index.js

--- src/components/ShowHide/index.js
+++ src/components/ShowHide/index.js
@@ -2,41 +2,41 @@ import {Component} from 'react'
 import './index.css'
 
 class ShowHide extends Component {
-  state = {isShowFirstName: false, isShowLastName: false}
-
-  showFirstName = () => {
-    this.setState(prevState => ({isShowFirstName: !prevState.isShowFirstName}))
-  }
-
-  showLastName = () => {
-    this.setState(prevState => ({isShowLastName: !prevState.isShowLastName}))
-  }
+  state = {showFirstname: false, showLastname: false}
 
   render() {
-    const {isShowFirstName, isShowLastName} = this.state
+    const {showFirstname, showLastname} = this.state
     return (
       <div className="bg-container">
         <h1 className="heading">Show/Hide</h1>
-        <div className="details-container">
-          <div className="firstname-container">
+        <div className="names-container">
+          <div>
             <button
-              type="button"
               className="button"
-              onClick={this.showFirstName}
+              type="button"
+              onClick={() => {
+                this.setState(prevState => ({
+                  showFirstname: !prevState.showFirstname,
+                }))
+              }}
             >
               Show/Hide Firstname
             </button>
-            {isShowFirstName ? <p className="para">Joe</p> : null}
+            {showFirstname && <p className="container">Joe</p>}
           </div>
-          <div className="lastname-container">
+          <div>
             <button
-              type="button"
               className="button"
-              onClick={this.showLastName}
+              type="button"
+              onClick={() => {
+                this.setState(prevState => ({
+                  showLastname: !prevState.showLastname,
+                }))
+              }}
             >
               Show/Hide Lastname
             </button>
-            {isShowLastName ? <p className="para">Jonas</p> : null}
+            {showLastname && <p className="container">Jonas</p>}
           </div>
         </div>
       </div>
